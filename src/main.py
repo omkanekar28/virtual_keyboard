ef986a3f39e68d447583bb4c3490cefb0c2670bc
Om Kanekar <omkanekar28@gmail.com>
2024-06-29 10:40:17 +0530

    [FEAT] Pressing of keys

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
-import logging
 import time
 import cv2
 import mediapipe as mp
-import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import logging
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -19,6 +23,7 @@ class VirtualKeyboard:
         self.start_time = time.time()
         self.frame_count = 0
         self.resolution = resolution
+        self.is_toggled = False
         
         # WEBCAM CONFIG
         self.webcam = cv2.VideoCapture(webcam)
@@ -31,12 +36,10 @@ class VirtualKeyboard:
         self.top_row_keys = "qwertyuiop"
         self.middle_row_keys = "asdfghjkl"
         self.second_middle_row_keys = "zxcvbnm,."
-        self.numbers_row_keys = "1234567890"
-        self.operators = "+-*/"
-        self.is_numbers_row_enabled = True
+        self.output = ""
 
         # KEY_POSITIONS
-        self.key_positions = list()
+        self.key_positions = dict()
 
     def show_webcam_with_hands(self):
         while True:
@@ -60,17 +63,13 @@ class VirtualKeyboard:
                     pos, size = self.draw_key(frame, top_left, bottom_right, 
                                               '<-')
                     if self.frame_count == 1:
-                        print(pos, size)
-                        #TODO: the print for 1st row gives different values 
-                        #TODO: than the one in exit(q) block, find out why.
-                        self.key_positions.append([pos, size])
+                        self.key_positions['<-'] = [pos, size]
                     continue
 
                 pos, size = self.draw_key(frame, top_left, bottom_right, 
                                           self.top_row_keys[key_top_row])
                 if self.frame_count == 1:
-                        print(pos, size)
-                        self.key_positions.append([pos, size])
+                        self.key_positions[self.top_row_keys[key_top_row]] = [pos, size]
 
                 top_left[0] += int(frame.shape[1] / 12)
                 bottom_right[0] += int(frame.shape[1] / 12)
@@ -89,13 +88,13 @@ class VirtualKeyboard:
                     pos, size = self.draw_key(frame, top_left, bottom_right, 
                                               '<--')
                     if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
+                        self.key_positions['<--'] = [pos, size]
                     continue
 
                 pos, size = self.draw_key(frame, top_left, bottom_right, 
                                           self.middle_row_keys[key_middle_row])
                 if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
+                        self.key_positions[self.middle_row_keys[key_middle_row]] = [pos, size]
 
                 top_left[0] += int(frame.shape[1] / 12)
                 bottom_right[0] += int(frame.shape[1] / 12)
@@ -111,58 +110,24 @@ class VirtualKeyboard:
                 pos, size = self.draw_key(frame, top_left, bottom_right, 
                                           self.second_middle_row_keys[key_second_middle_row])
                 if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
+                        self.key_positions[self.second_middle_row_keys[key_second_middle_row]] = [pos, size]
 
                 top_left[0] += int(frame.shape[1] / 12)
                 bottom_right[0] += int(frame.shape[1] / 12)
 
-            # 4TH ROW
+            # 4TH ROW (SPACEBAR)
             top_left = [int(frame.shape[1] / 20), int(frame.shape[0] / 6)]
             bottom_right = [int(frame.shape[1] / 9), int(frame.shape[0] / 4)]
             top_left[1] += 3 * int(frame.shape[0] / 8)
             bottom_right[1] += 3 * int(frame.shape[0] / 8)
-
             bottom_right[0] += int(frame.shape[1] / 1.5)
+            bottom_right[0] += int(frame.shape[1] / 5)
+
             pos, size = self.draw_key(frame, top_left, bottom_right, "spacebar")
             if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
+                        self.key_positions['spacebar'] = [pos, size]
 
             top_left[0] = bottom_right[0] + int(frame.shape[1] / 50)
-            bottom_right[0] += int(frame.shape[1] / 5)
-            pos, size = self.draw_key(frame, top_left, bottom_right, "123")
-            if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
-
-            # NUMBERS ROW
-            if self.is_numbers_row_enabled:
-                top_left = [int(frame.shape[1] / 20), int(frame.shape[0] / 6)]
-                bottom_right = [int(frame.shape[1] / 9), int(frame.shape[0] / 4)]
-                top_left[1] += 2 * int(frame.shape[0] / 4)
-                bottom_right[1] += 2 * int(frame.shape[0] / 4)
-
-                for key_numbers_row in range(10):
-
-                    pos, size = self.draw_key(frame, top_left, bottom_right, 
-                                              self.numbers_row_keys[key_numbers_row])
-                    if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
-
-                    top_left[0] += int(frame.shape[1] / 12)
-                    bottom_right[0] += int(frame.shape[1] / 12)
-                
-                top_left = [int(frame.shape[1] / 20), int(frame.shape[0] / 6)]
-                bottom_right = [int(frame.shape[1] / 9), int(frame.shape[0] / 4)]
-                top_left[1] += int(2.5 * int(frame.shape[0] / 4))
-                bottom_right[1] += int(2.5 * int(frame.shape[0] / 4))
-
-                for operator in self.operators:
-                    pos, size = self.draw_key(frame, top_left, bottom_right, 
-                                              operator)
-                    if self.frame_count == 1:
-                        self.key_positions.append([pos, size])
-
-                    top_left[0] += int(frame.shape[1] / 12)
-                    bottom_right[0] += int(frame.shape[1] / 12)
             
             # HAND DETECTION
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -180,22 +145,70 @@ class VirtualKeyboard:
                             x = int(landmark.x * frame.shape[1])
                             y = int(landmark.y * frame.shape[0])
                             cv2.circle(frame, (x, y), 5, (255, 0, 255), cv2.FILLED)
-            
-            for key_position in self.key_positions:
-                point1 = key_position[0]
-                point2 = key_position[1]
-                cv2.line(frame, point1, point2, (255, 255, 255), 2)
+                            if id == 8:
+                                self.index = (x, y)
+                            if id == 12:
+                                self.middle = (x, y)
+                for key in list(self.key_positions.keys()):
+                    top_left = self.key_positions[key][0]
+                    bottom_right = self.key_positions[key][1]
+                    index_on_key = self.is_point_inside_rectangle(
+                         top_left,
+                         bottom_right,
+                         self.index
+                    )
 
+                    middle_on_key = self.is_point_inside_rectangle(
+                        top_left,
+                        bottom_right,
+                        self.middle
+                    )
+
+                    if index_on_key and middle_on_key:
+                        cv2.rectangle(
+                             frame, 
+                             self.key_positions[key][0],
+                             self.key_positions[key][1], 
+                             (225, 255, 255),
+                             2
+                        )
+
+                        distance_between_fingers = euclidean_distances(
+                            [(self.index)],
+                            [(self.middle)]
+                        )
+
+                        if distance_between_fingers < 50:
+                            if self.is_toggled:
+                                continue
+                            self.toggle_key(
+                                frame, 
+                                self.key_positions[key][0],
+                                self.key_positions[key][1]
+                            )
+                            self.perform_action(key)
+                            self.is_toggled = True
+                        else:
+                            self.is_toggled = False
+
+            text_position = int((self.resolution[0])/2), int((self.resolution[1])/2)
+            cv2.putText(
+                frame, self.output, 
+                text_position, 
+                cv2.FONT_HERSHEY_SIMPLEX,
+                2,
+                (255, 255, 255), 
+                3
+            )                                       
             cv2.imshow("Webcam", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                    for i in self.key_positions:
-                         print(i)
                     break
 
         self.webcam.release()
         cv2.destroyAllWindows()
 
     def draw_key(self, frame, top_left, bottom_right, key):
+        
         # DRAWING RECTANGLE
         cv2.rectangle(frame, top_left, bottom_right, (255, 0, 0), 2)
 
@@ -210,7 +223,36 @@ class VirtualKeyboard:
         )
 
         # RETURNS POSITION
-        return [top_left, bottom_right]
+        return [top_left.copy(), bottom_right.copy()]
+    
+    def is_point_inside_rectangle(self, top_left, bottom_right, point):
+        x, y = point
+        if (top_left[0] <= x <= bottom_right[0]) and (top_left[1] <= y <= bottom_right[1]):
+            return True
+        return False
+    
+    def toggle_key(self, frame, top_left, bottom_right):
+        cv2.rectangle(
+            frame,
+            top_left,
+            bottom_right,
+            (0, 255, 0),
+            cv2.FILLED
+        )
+
+    def perform_action(self, key):
+        if key == '<-':
+            if self.output == '':
+                return
+            else:
+                self.output = self.output[:-1]
+        elif key == '<--':
+            # DO WHATEVER YOU WANT WITH THE TEXT AFTER PRESSING ENTER
+            self.output = ''
+        elif key == 'spacebar':
+            self.output += ' '
+        else:
+            self.output += key
 
     def start_process(self):
         try:
@@ -219,8 +261,6 @@ class VirtualKeyboard:
             self.show_webcam_with_hands()
             logger.info(f"Process finished with execution time of {time.time() - start} seconds")
         except Exception as e:
-            import traceback
-            traceback.print_exc()
             logger.error(str(e))
 
 
